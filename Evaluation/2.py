fe2b8b1b656027e6e74b6cad917e40eda14aa609
Badr Mubashir <77334849+mubaskid@users.noreply.github.com>
2021-04-21 09:16:09 +0100

    oit

--- Evaluation/2.py
+++ Evaluation/2.py
@@ -1,12 +1,13 @@
-def main():
-    fileVariable = open("array allocation", "w")
-    fileVariable.write(
-        "Memory allocation is the process",)
-    fileVariable.write("by which a program is assigned or allocated",)
-    fileVariable.write(
-        "to a particular empty block of space in computer memory.")
+# def main():
+#     fileVariable = open("array allocation", "w")
+#     fileVariable.write(
+#         "Memory allocation is the process",)
+#     fileVariable.write("by which a program is assigned or allocated",)
+#     fileVariable.write(
+#         "to a particular empty block of space in computer memory.")
 
-    print(fileVariable.write)
+#     print(fileVariable.write)
 
 
-main()
+# main()
+# or the memory is allocated when it is created
